efc472d38523a6e0c3fe26c84e5f000df5a373e9
renat999 <renat999@mit.edu>
2023-12-12 20:01:27 -0500

    Cleaned up driver file (diffusion-train-network.py)

--- diffusion-train-network.py
+++ diffusion-train-network.py
@@ -63,10 +63,6 @@ cs = lorenz['cs']
 t = lorenz['t']
 dt = t[1]-t[0]
 
-# SPECIFY IF YOU WANT TO TRAIN AND SAVE THE RESULTS
-train = False
-save = False
-
 ######################### Network parameters ##################################
 
 n_layers = 3 #Number of hidden layers
@@ -74,10 +70,10 @@ n_dim_per_layer = 10 #Neurons per layer
 
 n_dimensions = 3 #Spatial dimension 
 
-ACTIVATIONS = tf.nn.relu #Activation function
+ACTIVATIONS = tf.nn.leaky_relu #Activation function
 VALIDATION_SPLIT = .2 # 80% for training, 20% for testing
 BATCH_SIZE = 128
-LEARNING_RATE = 1e-2
+LEARNING_RATE = 1e-1
 N_EPOCHS = 50
 
 # use diagonal sigma matrix
@@ -92,256 +88,152 @@ encoder = ModelBuilder.diff_network(
                                     name="diff_net",
                                     activation=ACTIVATIONS,
                                     diffusivity_type=diffusivity_type)
-#encoder.summary()
+encoder.summary()
 
 #dictionary with jacobian parameters
 jac_par = {'sigma': 10, 'r': 28, 'beta': 8/3}
 
-if train:
-
-    # Prepare the input tensor
-    for n_point, nominal_point in enumerate(x_n):
+# Prepare the input tensor
+for n_point, nominal_point in enumerate(x_n):
+    
+    xn_matrix = np.tile(nominal_point,(1000,1))
+    
+    slice_start = int(n_point*1000)
+    slice_end = int(slice_start + 1000)
+    
+    x_tilde_n_slice = x_tilde_n[slice_start:slice_end,:]
+    x_tilde_n1_slice = x_tilde_np1[slice_start:slice_end,:]
+    
+    new_block = np.hstack((xn_matrix,x_tilde_n_slice,x_tilde_n1_slice))
+    
+    if n_point == 0:
         
-        xn_matrix = np.tile(nominal_point,(1000,1))
+        full_data = new_block
         
-        slice_start = int(n_point*1000)
-        slice_end = int(slice_start + 1000)
+    else:
         
-        x_tilde_n_slice = x_tilde_n[slice_start:slice_end,:]
-        x_tilde_n1_slice = x_tilde_np1[slice_start:slice_end,:]
+        full_data = np.vstack((full_data,new_block))
         
-        new_block = np.hstack((xn_matrix,x_tilde_n_slice,x_tilde_n1_slice))
-        
-        if n_point == 0:
-            
-            full_data = new_block
-            
-        else:
-            
-            full_data = np.vstack((full_data,new_block))
-            
-    n_pts = x_tilde_n.shape[0]
+n_pts = x_tilde_n.shape[0]
 
-    step_sizes = np.zeros(n_pts) + dt
-
-    model = SDEApproximationNetwork(sde_model=encoder,
-                                    step_size=dt,
-                                    jac_par=jac_par,
-                                    method="euler",
-                                    diffusivity_type=diffusivity_type)
-
-    model.compile(optimizer=tf.keras.optimizers.Adamax())
-
-    sde_i = SDEIdentification(model=model)
-
-    xn = full_data[:,0:3]
-    tilde_xn = full_data[:,3:6]
-    tilde_xn1 = full_data[:,6:9]
-
-    hist = sde_i.train_model(xn, tilde_xn, tilde_xn1, step_size=step_sizes,
-                            validation_split=VALIDATION_SPLIT,
-                            n_epochs=N_EPOCHS,
-                            batch_size=BATCH_SIZE)
-
-    plt.figure(16,figsize=(6,4))
-    plt.title(r"Diagonal $\Sigma$")
-    plt.plot(hist.history["loss"], label='Training')
-    plt.plot(hist.history["val_loss"], label='Validation')
-    plt.ylim([np.min(hist.history["loss"])*1.1, np.max(hist.history["loss"])])
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.savefig('training.png')
-    plt.show()
-
-    # plt.figure(17,figsize=(6,4))
-    # plt.title(r"Diagonal $\Sigma$")
-    # plt.plot(hist.history["accuracy"], label='Training')
-    # plt.plot(hist.history["val_accuracy"], label='Validation')
-    # plt.ylim([np.min(hist.history["accuracy"])*1.1, np.max(hist.history["accuracy"])])
-    # plt.xlabel("Epoch")
-    # plt.ylabel("Accuracy")
-    # plt.legend()
-    # plt.savefig('training-accuracy.png')
-    # plt.show()
-
-file_path = 'Trained_Dietrich'
-file_path += '/' + diffusivity_type + '/'
-file_path += f'HL{n_layers}_'
-file_path += f'N{n_dim_per_layer}_'
-file_path += 'LReLU_'
-file_path += 'LR1e-1_'
-file_path += f'BS{BATCH_SIZE}_'
-file_path += f'EP{N_EPOCHS}/'
-
-if save:
-    model.save_weights(file_path, overwrite=True, save_format=None, options=None)
-
-################## TEST
+step_sizes = np.zeros(n_pts) + dt
 
 model = SDEApproximationNetwork(sde_model=encoder,
-                                    step_size=dt,
-                                    jac_par=jac_par,
-                                    method="euler",
-                                    diffusivity_type=diffusivity_type)
+                                step_size=dt,
+                                jac_par=jac_par,
+                                method="euler",
+                                diffusivity_type=diffusivity_type)
 
-model.load_weights(file_path)
+model.compile(optimizer=tf.keras.optimizers.Adamax())
 
 sde_i = SDEIdentification(model=model)
 
-N = len(t)
-epsilon = 10e-3
-sigma = 10
-r = 28
-beta = 8/3
+xn = full_data[:,0:3]
+tilde_xn = full_data[:,3:6]
+tilde_xn1 = full_data[:,6:9]
 
-# initial conditions
-x_EM = np.zeros((N,3))
-xtilde = np.zeros((N,3))
-xtilde_NN = np.zeros((N,3))
-x_NN = np.zeros((N,3))
-x_alpha = np.zeros((N,3))
-x_EM[0,:] = [-8.67139571762,4.98065219709,25]
-x_NN[0,:] = [-8.67139571762,4.98065219709,25]
-x_alpha[0,:] = [-8.67139571762,4.98065219709,25]
+hist = sde_i.train_model(xn, tilde_xn, tilde_xn1, step_size=step_sizes,
+                        validation_split=VALIDATION_SPLIT,
+                        n_epochs=N_EPOCHS,
+                        batch_size=BATCH_SIZE)
 
-xtilde[0,:] = epsilon*np.random.randn(3)
-xtilde_NN[0,:] = xtilde[0,:]
-
-# time integration
-for n in range(N-1):
-    tn = t[n]
-    dW = np.sqrt(dt) * np.random.randn(N)
-
-    # Euler-Maruyama method
-    x_EM[n+1,:] = x_EM[n,:] + f(x_EM[n,:])*dt
-
-    # alpha method
-    alpha = 1.0
-    xtilde[n+1,:] = xtilde[n,:] + np.matmul(dfdx(x_alpha[n,:]),xtilde[n,:])*dt + naive_model(alpha)*dW[n]
-    x_alpha[n+1,:] = xh[n+1,:] + xtilde[n+1,:]
-
-    # NN method
-    xtilde_NN[n+1,:] = sde_i.sample_tilde_xn1(x_NN[n,:], xtilde_NN[n,:], dt, jac_par, diffusivity_type)
-    x_NN[n+1,:] = xh[n+1,:] + xtilde_NN[n+1,:]
-
-
-# plt.figure(3,figsize=(12,4))
-# plt.subplot(1,3,1)
-# plt.plot(t,np.log(np.abs(xtilde[:,0])),label=r"$\tilde{x}_\alpha$")
-# plt.plot(t,np.log(np.abs(xtilde_NN[:,0])),label=r"$\tilde{x}_{NN}$")
-# plt.xlabel("t")
-# plt.yscale("log")
-# #plt.title(r"$\alpha$ = " + str(alpha))
-# plt.ylabel(r"$\log(|\tilde{\mathbf{x}}|)$")
-# plt.legend()
-# plt.grid()
-
-# plt.subplot(1,3,2)
-# plt.plot(t,np.log(np.abs(xtilde[:,1])),label=r"$\tilde{y}_\alpha$")
-# plt.plot(t,np.log(np.abs(xtilde_NN[:,1])),label=r"$\tilde{y}_{NN}$")
-# plt.xlabel("t")
-# plt.yscale("log")
-# plt.ylabel(r"$\log(|\tilde{\mathbf{y}}|)$")
-# plt.legend()
-# plt.grid()
-
-# plt.subplot(1,3,3)
-# plt.plot(t,np.log(np.abs(xtilde[:,2])),label=r"$\tilde{z}_\alpha$")
-# plt.plot(t,np.log(np.abs(xtilde_NN[:,2])),label=r"$\tilde{z}_{NN}$")
-# plt.xlabel("t")
-# plt.yscale("log")
-# plt.ylabel(r"$\log(|\tilde{\mathbf{z}}|)$")
-# plt.legend()
-# plt.grid()
-# plt.tight_layout()
-# plt.savefig('logtildes.png', format='png', dpi=300)
-# plt.show()
-
-plt.figure(4,figsize=(12,4))
-plt.subplot(1,3,1)
-plt.plot(t,(xtilde[:,0]),label=r"$\tilde{x}_\alpha$")
-plt.plot(t,(xtilde_NN[:,0]),label=r"$\tilde{x}_{NN}$")
-plt.xlabel("t")
-#plt.title(r"$\alpha$ = " + str(alpha))
-plt.ylabel(r"$\tilde{\mathbf{x}}$")
+plt.figure(16,figsize=(6,4))
+plt.title(r"Diagonal $\Sigma$")
+plt.plot(hist.history["loss"], label='Training')
+plt.plot(hist.history["val_loss"], label='Validation')
+plt.ylim([np.min(hist.history["loss"])*1.1, np.max(hist.history["loss"])])
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
 plt.legend()
-plt.grid()
-
-plt.subplot(1,3,2)
-plt.plot(t,xtilde[:,1],label=r"$\tilde{y}_\alpha$")
-plt.plot(t,xtilde_NN[:,1],label=r"$\tilde{y}_{NN}$")
-plt.xlabel("t")
-plt.ylabel(r"$\tilde{\mathbf{y}}$")
-plt.legend()
-plt.grid()
-
-plt.subplot(1,3,3)
-plt.plot(t,xtilde[:,2],label=r"$\tilde{z}_\alpha$")
-plt.plot(t,xtilde_NN[:,2],label=r"$\tilde{z}_{NN}$")
-plt.xlabel("t")
-plt.ylabel(r"$\tilde{\mathbf{z}}$")
-plt.legend()
-plt.grid()
-plt.tight_layout()
-#plt.savefig('tildes.png', format='png', dpi=300)
+plt.savefig('training.png')
 plt.show()
 
-fig = plt.figure(figsize=(7,7))
-ax1 = fig.add_subplot(1,1,1,projection='3d')
-ax1.plot(*xtilde.T,label=r"$\alpha$")
-ax1.set_xlabel(r"$\tilde{x}_\alpha$")
-ax1.set_ylabel(r"$\tilde{y}_\alpha$")
-ax1.set_zlabel(r"$\tilde{z}_\alpha$")
-ax1.legend()
-#plt.savefig('alpha-3d.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
-plt.show()
+################## TEST
 
-fig2 = plt.figure(figsize=(7,7))
-ax12 = fig2.add_subplot(1,1,1,projection='3d')
-ax12.plot(*xtilde_NN.T,label=r"$NN$")
-ax12.set_xlabel(r"$\tilde{x}_{NN}$")
-ax12.set_ylabel(r"$\tilde{y}_{NN}$")
-ax12.set_zlabel(r"$\tilde{z}_{NN}$")
-#ax12.legend()
-plt.savefig('nn-3d.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
-plt.show()
+plot = True
 
+if plot:
+    N = len(t)
+    epsilon = 10e-3
+    sigma = 10
+    r = 28
+    beta = 8/3
 
+    # initial conditions
+    x_EM = np.zeros((N,3))
+    xtilde = np.zeros((N,3))
+    xtilde_NN = np.zeros((N,3))
+    x_NN = np.zeros((N,3))
+    x_alpha = np.zeros((N,3))
+    x_EM[0,:] = [-8.67139571762,4.98065219709,25]
+    x_NN[0,:] = [-8.67139571762,4.98065219709,25]
+    x_alpha[0,:] = [-8.67139571762,4.98065219709,25]
 
-# plt.figure(22,figsize=(12,4))
-# plt.subplot(1,3,1)
-# plt.plot(t,xh[:,0],label=r"${x}$")
-# plt.plot(t,x_EM[:,0],label=r"${x}_{EM}$")
-# plt.plot(t,x_NN[:,0],label=r"${x}_{NN}$")
-# #plt.plot(t,xtilde[:,0],label=r"$\tilde{x}_{\alpha}$")
-# plt.legend()
-# plt.xlabel("t")
-# plt.ylabel(r"${\mathbf{x}}$")
-# plt.ylim(-20,20)
-# plt.grid()
+    xtilde[0,:] = epsilon*np.random.randn(3)
+    xtilde_NN[0,:] = xtilde[0,:]
 
-# plt.subplot(1,3,2)
-# plt.plot(t,xh[:,1],label=r"${y}$")
-# plt.plot(t,x_EM[:,1],label=r"${y}_{EM}$")
-# plt.plot(t,x_NN[:,1],label=r"${y}_{NN}$")
-# #plt.plot(t,xtilde[:,1],label=r"$\tilde{y}_{\alpha}$")
-# plt.legend()
-# plt.xlabel("t")
-# plt.ylabel(r"${\mathbf{y}}$")
-# plt.ylim(-25,25)
-# plt.grid()
+    # time integration
+    for n in range(N-1):
+        tn = t[n]
+        dW_n = np.sqrt(dt) * np.random.randn(3)
 
-# plt.subplot(1,3,3)
-# plt.plot(t,xh[:,2],label=r"${z}$")
-# plt.plot(t,x_EM[:,2],label=r"${z}_{EM}$")
-# plt.plot(t,x_NN[:,2],label=r"${z}_{NN}$")
-# #plt.plot(t,xtilde[:,2],label=r"$\tilde{z}_{\alpha}$")
-# plt.xlabel("t")
-# plt.ylabel(r"${\mathbf{z}}$")
-# plt.legend()
-# plt.grid()
-# plt.ylim(0,50)
-# plt.tight_layout()
-# plt.savefig('paths.png', format='png', dpi=300)
-# plt.show()
+        # Euler-Maruyama method
+        x_EM[n+1,:] = x_EM[n,:] + f(x_EM[n,:])*dt
+
+        # alpha method
+        alpha = 1.0
+        xtilde[n+1,:] = xtilde[n,:] + np.matmul(dfdx(x_alpha[n,:]),xtilde[n,:])*dt + naive_model(alpha)*dW_n
+        x_alpha[n+1,:] = xh[n+1,:] + xtilde[n+1,:]
+
+        # NN method
+        xtilde_NN[n+1,:] = sde_i.sample_tilde_xn1(x_NN[n,:], xtilde_NN[n,:], dt, jac_par, diffusivity_type)
+        x_NN[n+1,:] = xh[n+1,:] + xtilde_NN[n+1,:]
+
+    plt.figure(4,figsize=(12,4))
+    plt.subplot(1,3,1)
+    plt.plot(t,(xtilde[:,0]),label=r"$\tilde{x}_\alpha$")
+    plt.plot(t,(xtilde_NN[:,0]),label=r"$\tilde{x}_{NN}$")
+    plt.xlabel("t")
+    #plt.title(r"$\alpha$ = " + str(alpha))
+    plt.ylabel(r"$\tilde{\mathbf{x}}$")
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(1,3,2)
+    plt.plot(t,xtilde[:,1],label=r"$\tilde{y}_\alpha$")
+    plt.plot(t,xtilde_NN[:,1],label=r"$\tilde{y}_{NN}$")
+    plt.xlabel("t")
+    plt.ylabel(r"$\tilde{\mathbf{y}}$")
+    plt.legend()
+    plt.grid()
+
+    plt.subplot(1,3,3)
+    plt.plot(t,xtilde[:,2],label=r"$\tilde{z}_\alpha$")
+    plt.plot(t,xtilde_NN[:,2],label=r"$\tilde{z}_{NN}$")
+    plt.xlabel("t")
+    plt.ylabel(r"$\tilde{\mathbf{z}}$")
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    #plt.savefig('tildes.png', format='png', dpi=300)
+    plt.show()
+
+    fig2 = plt.figure(figsize=(7,7))
+    ax12 = fig2.add_subplot(1,1,1,projection='3d')
+    ax12.plot(*xtilde_NN.T,label=r"$NN$")
+    ax12.set_xlabel(r"$\tilde{x}_{NN}$")
+    ax12.set_ylabel(r"$\tilde{y}_{NN}$")
+    ax12.set_zlabel(r"$\tilde{z}_{NN}$")
+    #ax12.legend()
+    #plt.savefig('nn-3d.png',dpi=300,format='png',transparent=True)
+    plt.show()
+
+    fig22 = plt.figure(figsize=(7,7))
+    ax122 = fig22.add_subplot(1,1,1,projection='3d')
+    ax122.plot(*x_NN.T,label=r"$NN$")
+    ax122.set_xlabel(r"$x + \tilde{x}_{NN}$")
+    ax122.set_ylabel(r"$y + \tilde{y}_{NN}$")
+    ax122.set_zlabel(r"$z + \tilde{z}_{NN}$")
+    #ax122.legend()
+    plt.savefig('nn-3d-full.png',dpi=300,format='png',transparent=True)
+    plt.show()
